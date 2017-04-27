086fca9241d7cab787fc1505360a2e81a021e719
Rohit208 <rohitrehni.rr@gmail.com>
2017-04-27 20:11:52 +0530

    dataStructure

--- Script1/Structure.pl
+++ Script1/Structure.pl
@@ -4,44 +4,44 @@ use strict;
 use warnings;
 use diagnostics;
 use Data::Dumper;
+
+my $data = {
+	"Cricket" => ['a|india', 'b|us', 'c|canada', 'f|india', 'z|us', 'g|india','p|india'],
+	"Football" => ['c|india', 'd|us', 'e|canada','fgh|canada','dfg|us','kjh|us'],
+};
+
+my $expected_ds = {
+
+	india => {
+		"Cricket" => ['a', 'd'],
+	    "Football" => ['c']	
+	},
+
+};
+
+my %builded_ds;
+
+foreach my $keys (keys %$data){
+   my %hash = map {my ( $v,$k ) = split ('\|');  ($k,$v) } @{$data->{$keys}};
+   $builded_ds{$keys} = \%hash;
+}
+print Dumper $expected_ds;
+print "==============================\n";
+print Dumper $data;
+print "==============================\n";
+my $cons_ds;
+ 
+
+foreach my $game (keys %$data){
+	foreach my $val (@{$data->{$game}}) {
+		my ($player, $country) = split '\|', $val;
+		if(exists $cons_ds->{$country}{$game}){
+	    	$cons_ds->{$country}{$game}  = [ref($cons_ds->{$country}{$game})?@{$cons_ds->{$country}{$game}}:$cons_ds->{$country}{$game},$player];
+	    	next;
+		}
+		$cons_ds->{$country}{$game} = $player;
+	}
+} 
+
+print Dumper $cons_ds;
 $| = 1;
-
-my $countries = {
-    "Cricket" => {
-        india  => ['a','d'],
-        us     => 'b',
-        canada => 'c',
-    },
-    "football" => {
-        india  => 'a',
-        us     => 'b',
-        canada => 'c',
-        india  => 'd'
-      }
-
-};
-
-print $countries->{'football'}->{'india'} = "sachin";
-print "\n";
-
-my $countries2 = {
-    "Australia" => {
-        Cricket  => 'a',
-        Hockey   => 'b',
-        Football => 'c'
-    },
-    "India" => {
-        Cricket  => 'a',
-        Hockey => 'b',
-        Football => 'c'
-      }
-
-};
-
-print $countries2->{'India'}->{'GYMnastics'} = 'g';
-print "\n";
-
-print Dumper $countries2;
-print "==========================================\n";
-print Dumper $countries;
-
